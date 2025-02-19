091b8b8df9d53a71b370cd2dde8484a5aa0950b9
zongyanan <yanan_zong@huace.cn>
2025-02-19 17:16:02 +0800

    第一次提交

---
+++ src/test/java/com/kio/spingai/SpingAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kio.spingai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
